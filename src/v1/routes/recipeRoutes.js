9db3e61a24dc04bf56a652593efb5afd9173913c
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2023-09-28 23:41:22 -0400

    implem-apicache

--- src/v1/routes/recipeRoutes.js
+++ src/v1/routes/recipeRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const recipeController = require("../controllers/recipeController");
 const router = express.Router();
+const apicache = require("apicache");
+
+// Cache
+const cache = apicache.middleware;
 
 // CRUD for recipes
-router.get("/", recipeController.getAllRecipes);
+router.get("/", cache("2 minutes"), recipeController.getAllRecipes);
 router.get("/:recipeId", recipeController.getOneRecipe);
 
 router.post("/", recipeController.createNewRecipe);
